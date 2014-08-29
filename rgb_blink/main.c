f0effd42ca16601d97e434bfa55b0ac04558580d
Frank Holtz <frank-github2013@holtznet.de>
2014-08-29 07:32:28 +0200

    Blink example bugfix

--- rgb_blink/main.c
+++ rgb_blink/main.c
@@ -1,74 +1,136 @@
-/* Copyright (c) 2009 Nordic Semiconductor. All Rights Reserved.
- *
- * The information contained herein is property of Nordic Semiconductor ASA.
- * Terms and conditions of usage are described in detail in NORDIC
- * SEMICONDUCTOR STANDARD SOFTWARE LICENSE AGREEMENT.
- *
- * Licensees are granted free, non-transferable use of the information. NO
- * WARRANTY of ANY KIND is provided. This heading must NOT be removed from
- * the file.
+/*
+ * This is an LED blinking example
+ * Its support RGB or up to eigt single color LEDs
+ * High drive is enabled. A maximum current of 15mA for all enabled ports are available (3*5mA .. 15*1mA)
  *
  */
 
-/**
-* @example Board/pca10003/blinky_example/main.c
-*
-* @brief Blinky Example Application main file.
-*
-* This file contains the source code for a sample application using GPIO to drive LEDs.
-*
-*/
-
-#include <stdbool.h>
-#include <stdint.h>
-#include "nrf_delay.h"
-#include "nrf_gpio.h"
 #include "boards.h"
+#include "nrf_gpio.h"
+#include "nrf_delay.h"
 
 /**
- * @brief Function for application main entry.
+ * Configure a pin as high drive output
+ */
+static __INLINE void configure_pin(uint32_t pin)
+{
+    NRF_GPIO->PIN_CNF[pin]  = (GPIO_PIN_CNF_SENSE_Disabled << GPIO_PIN_CNF_SENSE_Pos)
+                            | (GPIO_PIN_CNF_DRIVE_H0H1 << GPIO_PIN_CNF_DRIVE_Pos)
+                            | (GPIO_PIN_CNF_PULL_Disabled << GPIO_PIN_CNF_PULL_Pos)
+                            | (GPIO_PIN_CNF_INPUT_Connect << GPIO_PIN_CNF_INPUT_Pos)
+                            | (GPIO_PIN_CNF_DIR_Output << GPIO_PIN_CNF_DIR_Pos);
+}
+
+
+/**
+ * Main function
  */
 int main(void)
 {
-    // Configure LED-pins as outputs.
-    nrf_gpio_cfg_output(LED_RGB_RED);
-    NRF_GPIO->PIN_CNF[LED_RGB_RED] = (GPIO_PIN_CNF_SENSE_Disabled << GPIO_PIN_CNF_SENSE_Pos)
-                             | (GPIO_PIN_CNF_DRIVE_H0H1 << GPIO_PIN_CNF_DRIVE_Pos)
-                             | (GPIO_PIN_CNF_PULL_Disabled << GPIO_PIN_CNF_PULL_Pos)
-                             | (GPIO_PIN_CNF_INPUT_Connect << GPIO_PIN_CNF_INPUT_Pos)
-                             | (GPIO_PIN_CNF_DIR_Output << GPIO_PIN_CNF_DIR_Pos);
+    // Configure Pins as HIGH drive Output
+    #ifdef LED_RGB_RED
+     configure_pin(LED_RGB_RED);
+    #endif
 
-    nrf_gpio_cfg_output(LED_RGB_GREEN);
-    NRF_GPIO->PIN_CNF[LED_RGB_GREEN] = (GPIO_PIN_CNF_SENSE_Disabled << GPIO_PIN_CNF_SENSE_Pos)
-                             | (GPIO_PIN_CNF_DRIVE_H0H1 << GPIO_PIN_CNF_DRIVE_Pos)
-                             | (GPIO_PIN_CNF_PULL_Disabled << GPIO_PIN_CNF_PULL_Pos)
-                             | (GPIO_PIN_CNF_INPUT_Connect << GPIO_PIN_CNF_INPUT_Pos)
-                             | (GPIO_PIN_CNF_DIR_Output << GPIO_PIN_CNF_DIR_Pos);
-
-    nrf_gpio_cfg_output(LED_RGB_BLUE);
-    NRF_GPIO->PIN_CNF[LED_RGB_BLUE] = (GPIO_PIN_CNF_SENSE_Disabled << GPIO_PIN_CNF_SENSE_Pos)
-                             | (GPIO_PIN_CNF_DRIVE_H0H1 << GPIO_PIN_CNF_DRIVE_Pos)
-                             | (GPIO_PIN_CNF_PULL_Disabled << GPIO_PIN_CNF_PULL_Pos)
-                             | (GPIO_PIN_CNF_INPUT_Connect << GPIO_PIN_CNF_INPUT_Pos)
-                             | (GPIO_PIN_CNF_DIR_Output << GPIO_PIN_CNF_DIR_Pos);
-
-    char c=0;
-
-    // LED 0 and LED 1 blink alternately.
-    while(true)
-    {
-        if (c & 1) nrf_gpio_pin_clear(LED_RGB_RED);
-         else nrf_gpio_pin_set(LED_RGB_RED);
-
-        if (c & 2) nrf_gpio_pin_clear(LED_RGB_GREEN);
-         else nrf_gpio_pin_set(LED_RGB_GREEN);
+    #ifdef LED_RGB_GREEN
+     configure_pin(LED_RGB_GREEN);
+    #endif
     
+    #ifdef LED_RGB_BLUE
+     configure_pin(LED_RGB_BLUE);
+    #endif
 
-        if (c & 4) nrf_gpio_pin_clear(LED_RGB_BLUE);
-         else nrf_gpio_pin_set(LED_RGB_BLUE);
+    #ifdef LED_0
+     configure_pin(LED_0);
+    #endif
 
-        nrf_delay_ms(500);
-        c++;
+    #ifdef LED_1
+     configure_pin(LED_1);
+    #endif
+    
+    #ifdef LED_2
+     configure_pin(LED_2);
+    #endif
+
+    #ifdef LED_3
+     configure_pin(LED_3);
+    #endif
+    
+    #ifdef LED_4
+     configure_pin(LED_4);
+    #endif
+
+    #ifdef LED_5
+     configure_pin(LED_5);
+    #endif
+    
+    #ifdef LED_6
+     configure_pin(LED_6);
+    #endif
+
+    #ifdef LED_7
+     configure_pin(LED_7);
+    #endif
+    
+    // endless loop
+    for(char c=0;;c++)
+    {
+    #ifdef LED_RGB_RED
+      if (c & 1) nrf_gpio_pin_clear(LED_RGB_RED);
+       else nrf_gpio_pin_set(LED_RGB_RED);
+    #endif
+     
+    #ifdef LED_RGB_GREEN
+      if (c & 2) nrf_gpio_pin_clear(LED_RGB_GREEN);
+       else nrf_gpio_pin_set(LED_RGB_GREEN);
+    #endif    
+
+    #ifdef LED_RGB_BLUE
+      if (c & 4) nrf_gpio_pin_clear(LED_RGB_BLUE);
+       else nrf_gpio_pin_set(LED_RGB_BLUE);
+    #endif    
+
+    #ifdef LED_0
+      if (c & 1) nrf_gpio_pin_clear(LED_0);
+       else nrf_gpio_pin_set(LED_0);
+    #endif
+
+    #ifdef LED_1
+      if (c & 2) nrf_gpio_pin_clear(LED_1);
+       else nrf_gpio_pin_set(LED_1);
+    #endif
+
+    #ifdef LED_2
+      if (c & 4) nrf_gpio_pin_clear(LED_2);
+       else nrf_gpio_pin_set(LED_2);
+    #endif
+
+    #ifdef LED_3
+      if (c & 8) nrf_gpio_pin_clear(LED_3);
+       else nrf_gpio_pin_set(LED_3);
+    #endif
+
+    #ifdef LED_4
+      if (c & 16) nrf_gpio_pin_clear(LED_3);
+       else nrf_gpio_pin_set(LED_3);
+    #endif
+
+    #ifdef LED_5
+      if (c & 32) nrf_gpio_pin_clear(LED_5);
+       else nrf_gpio_pin_set(LED_5);
+    #endif
+
+    #ifdef LED_6
+      if (c & 64) nrf_gpio_pin_clear(LED_6);
+       else nrf_gpio_pin_set(LED_6);
+    #endif
+
+    #ifdef LED_7
+      if (c & 128) nrf_gpio_pin_clear(LED_7);
+       else nrf_gpio_pin_set(LED_7);
+    #endif
+
+      nrf_delay_ms(500);
     }
 }
 
